b84ccc5d05a191fc582303e979adb4a55c8948a2
Gernot Leitgab <gernot@leitgab.at>
2024-09-13 05:02:15 +0200

    [TASK] replace TYPO3_MODE constants

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,30 +1,21 @@
 <?php
 
-/*
- * This file is part of the advanced_cache extension for TYPO3 CMS.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
+declare(strict_types=1);
 
-call_user_func(function ($_EXTKEY) {
+defined('TYPO3') or die();
+
+(static function (): void {
+    $_EXTKEY = 'advanced_cache';
+
     if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_advancedcache_queue'])) {
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_advancedcache_queue'] = [];
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['tx_advancedcache_queue']['frontend'] =
             \WebentwicklerAt\AdvancedCache\Cache\Frontend\VariableFrontend::class;
     }
 
-    if (TYPO3_MODE === 'BE') {
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\DataHandling\DataHandler::class] = [
-            'className' => \WebentwicklerAt\AdvancedCache\Xclass\DataHandler::class,
-        ];
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][$_EXTKEY] =
-            \WebentwicklerAt\AdvancedCache\Hooks\PageRenderer::class . '->addInlineJavaScript';
-    }
-}, 'advanced_cache');
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\DataHandling\DataHandler::class] = [
+        'className' => \WebentwicklerAt\AdvancedCache\Xclass\DataHandler::class,
+    ];
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][$_EXTKEY] =
+        \WebentwicklerAt\AdvancedCache\Hooks\PageRenderer::class . '->addInlineJavaScript';
+})();
--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,10 @@
 <?php
-defined('TYPO3_MODE') or die();
 
-(function () {
+declare(strict_types=1);
+
+defined('TYPO3') or die();
+
+(static function (): void {
     $GLOBALS['TYPO3_CONF_VARS']['BE']['ContextMenu']['ItemProviders']['advanced_cache'] =
         \WebentwicklerAt\AdvancedCache\ContextMenu\ItemProvider::class;
 })();
